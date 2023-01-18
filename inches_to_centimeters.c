fff17cde09c8e05fc91645bb31020be75fe19e9b
KanchustambhamPavanCharishma <119915157+KanchustambhamPavanCharishma@users.noreply.github.com>
2023-01-18 11:45:28 +0530

    #include<stdio.h> void fun(int h) {     float f;     f=2.54*h;     printf("%0.2f",f); } int main() { 	int h; 	scanf("%d",&h); 	fun(h); }

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+void fun(int h)
+{
+    float f;
+    f=2.54*h;
+    printf("%0.2f",f);
+}
 int main()
 {
-    int h;
-    scanf("%d",&h);
-    float v;
-    v=2.54*h;
-    printf("%0.2f",v);
+	int h;
+	scanf("%d",&h);
+	fun(h);
 }
