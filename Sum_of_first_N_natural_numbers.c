e82f7122f97305a2bc7b2072a452be0b1fdb1e04
KanchustambhamPavanCharishma <119915157+KanchustambhamPavanCharishma@users.noreply.github.com>
2023-01-22 12:09:40 +0530

    #include<stdio.h> void fun(int n,int i,int num=0) {    for(i=0;i<=n;i++)     {         num=num+i;     }     printf("%d",num); } int main() {     int n,i,num=0;     scanf("%d",&n);     fun(n,i); }

--- Sum_of_first_N_natural_numbers.c
+++ Sum_of_first_N_natural_numbers.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
+void fun(int n,int i,int num=0)
+{
+   for(i=0;i<=n;i++)
+    {
+        num=num+i;
+    }
+    printf("%d",num); 
+}
 int main()
 {
     int n,i,num=0;
     scanf("%d",&n);
-    for(i=0;i<=n;i++)
-    {
-        num=num+i;
-    }
-    printf("%d",num);
+    fun(n,i);
 }
