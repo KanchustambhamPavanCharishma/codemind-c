afa707cf0fd44e8d75b30ac8a00fdeee6b458b6c
KanchustambhamPavanCharishma <119915157+KanchustambhamPavanCharishma@users.noreply.github.com>
2023-05-21 09:09:04 +0530

    #include<stdio.h> int main() {     int t,n,m;     scanf("%d",&t);     while(t--)     {         scanf("%d%d",&n,&m);         printf("%d ",n+m);     } }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int n,i;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		int a,b,c;
-		scanf("%d %d",&a,&b);
-		c=a+b;
-		printf("%d
-",c);
-	}
+    int t,n,m;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%d",&n,&m);
+        printf("%d
+",n+m);   
+    }
 }
